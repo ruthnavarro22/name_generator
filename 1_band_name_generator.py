3bf05050dccbd7d6292751e62dcf49ce1aeb50cb
Ruth Navarro <ruthsssy@gmail.com>
2023-10-29 13:06:48 +0100

    Initial Commit

---
+++ 1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+name = input("What is the name of the city you grew up? ")
+pet = input("What is your pet`s name? ")
+print("Your band name could be " + name + " " + pet)
